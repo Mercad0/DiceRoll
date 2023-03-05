f5b786934fd443686f751f7777d582eca3a8a0a5
Abimael M <abimaelmercado05@gmail.com>
2023-03-04 20:45:26 -0500

    Dice Roll App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageView1.image = dices.randomElement() //  can use as well [Int.random(in : 0...5)]
+        diceImageView2.image = dices.randomElement()
+        
+        
     }
-
-
 }
 
